ca5cf1bf127dac34f1b3ad881c1484dab65fc681
Igor Plotnikov <ig.plotnikov@gmail.com>
2024-07-10 13:06:38 +0300

    Added drug show and index views

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,5 +1,22 @@
 class DrugsController < ApplicationController
+  before_action :set_drug, only: %i[show]
+
   def index
     @drugs = Drug.all
   end
+
+  def show
+    @sgtins = @drug.sgtin.all
+  end
+end
+
+
+private
+
+def drugs_params
+  params.require(:drug).permit(:name)
+end
+
+def set_drug
+  @drug = Drug.find_by(params[:id])
 end
--- app/models/sgtin.rb
+++ app/models/sgtin.rb
@@ -2,4 +2,13 @@ class Sgtin < ApplicationRecord
   belongs_to :drug
   belongs_to :batch
   belongs_to :firm
+
+  def formatted_status_date
+    status_date.strftime('%d.%m.%Y')
+  end
+
+  def formatted_last_operation_date
+    last_operation_date.strftime('%d.%m.%Y')
+  end
+
 end
