bd55673baed5a7da4d64a04d418dca40019e29b5
Igor Plotnikov <ig.plotnikov@gmail.com>
2024-07-14 14:06:52 +0300

    Added full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "Packs"
+
+    if page_title.present?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+  end
 end
